1bf366c88c8a14e833b6479060b7cc248aef97f3
Cade Murphy <cademurphynz@gmail.com>
2016-03-08 10:12:39 +1300

    Minified Form.js as a test

--- _Astral/stage.php
+++ _Astral/stage.php
@@ -6,7 +6,4 @@ require '../app/app/bin/dev.php';
 // 1. Run Astral array creation
 //require 'start.php';
 
-// 2. Merge css
-$view = '../app/views/views/job.html';
-
-dd($cssarray);
+// 2. Merge Form.js
