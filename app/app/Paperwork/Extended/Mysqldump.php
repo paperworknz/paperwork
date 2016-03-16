6d6bc97914cc3d039a149e2afd9332ee592084d6
Cade Murphy <cademurphynz@gmail.com>
2016-03-17 12:19:38 +1300

    Mysqldump class
    Likely change this name to a more generic BUDR class

---
+++ app/app/Paperwork/Extended/Mysqldump.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Paperwork\Extended;
+
+class Mysqldump {
+	
+	protected $username;
+	protected $password;
+	
+	public function __construct(){
+		$this->username = $_ENV['DB_USER'];
+		$this->password = $_ENV['DB_PASSWORD'];
+	}
+	
+	public function backup(){
+		$app = \Slim\Slim::getInstance();
+		
+		$db = $_ENV['DB_PREFIX'].$app->user['username'];
+		die(
+			'mysqldump'
+			.' -u '.$this->username
+			.' -p'.$this->password
+			.' '.$db
+			.' > "/var/www/paperwork/app/app/storage/clients/NailedItConstruc/sql/logan.sql"'
+		);
+	}
+	
+}
