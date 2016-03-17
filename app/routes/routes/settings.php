f8c22c757e1b2fb32539899ffabf3f79e6b988f2
Cade Murphy <cademurphynz@gmail.com>
2016-03-17 16:15:25 +1300

    Soft deletes and settings.trash

--- app/routes/routes/settings.php
+++ app/routes/routes/settings.php
@@ -7,10 +7,18 @@ $app->get('/settings', 'uac', function() use ($app){
 	$my		= $app->sql->get('settings')->run();
 	$status	= $app->sql->get('job_status')->all()->run();
 	unset($status[0]); // Remove the in-built completed status
+	$job	= $app->sql->get('job')->softOnly()->all()->run();
+	$form	= $app->sql->get('job_form')->softOnly()->all()->run();
+	$client	= $app->sql->get('client')->softOnly()->all()->run();
+	$inv	= $app->sql->get('inv')->softOnly()->all()->run();
 	
 	$app->build->page('views/settings.html', [
 		'my'		=> $my,
 		'status'	=> $status,
+		'jobs'		=> $job,
+		'forms'		=> $form,
+		'clients'	=> $client,
+		'inv'		=> $inv,
 		'enableEmail' => false,
 	]);
 });
