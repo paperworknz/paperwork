3d20d5578343f922a6a166d72505c4429a6a5397
Cade Murphy <cademurphynz@gmail.com>
2016-03-21 13:59:06 +1300

    Testing Dropbox on live server

--- app/routes/other/get/pdf.php
+++ app/routes/other/get/pdf.php
@@ -33,3 +33,32 @@ $app->get('/get/pdf/:a/:b', 'uac', function($a, $b) use ($app){
 	}
 	
 });
+
+$app->get('/get/test/:a/:b', 'uac', function($a, $b) use ($app){
+	/* Methods */
+	
+	/* Construction */
+	$easy = $app->user['easy'];
+	$total = "/var/www/Dropbox/Paperwork/{$easy}/PDF/{$a}/{$b}";
+	if(file_exists($total)){
+		// DOWNLOAD //
+		$response = new Response(file_get_contents($total), 200, [
+			'Content-Description'	=> 'File Transfer',
+			'Content-Disposition'	=> 'inline; filename="'.$b.'"',
+			'Content-Transfer-Encoding' => 'binary',
+			'Content-Type'	=> 'application/pdf'
+		]);
+		$response->send(); // Return to client
+	}else{
+		$app->event->log([
+			'title' => $app->user['username'].' requested a missing PDF',
+			'text' => 'URI: '.$app->request->getResourceUri(),
+			'number' => 77,
+			'level' => 'Warning',
+			'uacID' => $app->user['uacID'],
+		]);
+		$app->flash('error', 'Sorry, we couldn\'t find that PDF.');
+		$app->redirect($app->root.'/job/'.$a);
+	}
+	
+});
--- app/routes/other/post/database.php
+++ app/routes/other/post/database.php
@@ -6,7 +6,7 @@ $app->post('/post/database', function() use ($app){
 	/* Construction */
 	$username = $_POST['username'];
 	$raw = file_get_contents('../app/app/resources/db_schema/sql_schema.sql'); // Load SQL structure script
-	$raw = str_replace('{{database}}', $_ENV['DB_PREFIX'].$username, $raw); // Replace placeholder with `app_$username`
+	$raw = str_replace('{{schema}}', $_ENV['DB_PREFIX'].$username, $raw); // Replace placeholder with `app_$username`
 	
 	try{
 		$app->pdo->master->exec($raw);
