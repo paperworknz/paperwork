63bf016e235f42a23c61b3e54949456ae842eb74
Cade Murphy <cademurphynz@gmail.com>
2016-04-08 14:03:26 +1200

    Add IP event log on home page land

--- app/routes/routes/home.php
+++ app/routes/routes/home.php
@@ -4,6 +4,15 @@ $app->get('/', 'app', function() use($app){
 	/* Methods */
 	
 	/* Construction */
+	$IP = $_SERVER['REMOTE_ADDR'];
+	
+	if($IP != '::1'){
+		$app->event->log([
+			'number' => 0,
+			'title' => 'Landing Page',
+			'text' => 'IP: '.$IP
+		]);
+	}
 	$app->build->page('views/home.html', [
 		'register' => false
 	]);
