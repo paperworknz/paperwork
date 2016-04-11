5fcda4ac05f0f10cc19e9d80a73c9af5a439d0a2
Cade Murphy <cademurphynz@gmail.com>
2016-04-11 12:03:47 +1200

    Move landing page event logging to the login page

--- app/routes/routes/login.php
+++ app/routes/routes/login.php
@@ -4,5 +4,20 @@ $app->get('/login', 'app', function() use ($app){
 	/* Methods */
 
 	/* Construction */
+	if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+		$IP = $_SERVER['HTTP_CLIENT_IP'];
+	} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+		$IP = $_SERVER['HTTP_X_FORWARDED_FOR'];
+	} else {
+		$IP = $_SERVER['REMOTE_ADDR'];
+	}
+	
+	if($IP != '::1' && $IP != '127.0.0.1'){
+		$app->event->log([
+			'number' => 0,
+			'title' => 'Landing Page',
+			'text' => 'IP: '.$IP
+		]);
+	}
 	$app->build->page('views/login.html');
 });
