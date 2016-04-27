9ca46375272f9eddb4f26fe358e603229ac18708
Cade Murphy <cademurphynz@gmail.com>
2016-04-27 12:33:09 +1200

    Flash message/redirect for sign up button

--- app/routes/routes/register.php
+++ app/routes/routes/register.php
@@ -5,6 +5,7 @@ $app->get('/register', 'app', function() use ($app){
 
 	/* Construction */
 	if(!$app->env['user_registration']){
+		$app->flash('info', 'Please email us at <a href="mailto:hello@paperwork.nz">hello@paperwork.nz</a> if you\'re interested in a free trial, we\'re looking for people just like you!');
 		$app->redirect($app->root);
 	}
 	$app->build->page('views/register.html');
