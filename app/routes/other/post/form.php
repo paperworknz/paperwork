24c996d313ae101766a6e80b3ae5668159ac506a
Cade Murphy <cademurphynz@gmail.com>
2016-06-07 15:55:41 +1200

    Fix pdf issue on new forms
    Making a new form didn't print the job number into the pdf post form.
    Resolved by adding the $job object into the form when it's built on
    /post/form

--- app/routes/other/post/form.php
+++ app/routes/other/post/form.php
@@ -32,6 +32,7 @@ $app->post('/post/form', 'uac', function() use ($app){
 			$html = $app->build->page('other/html/form.html', [
 				'html' => $template['content'],
 				'email' => $email,
+				'job' => $job,
 			], false);
 			
 			$json = [
