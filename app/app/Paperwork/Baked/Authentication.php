d427ac6699261a9a4122445b110d07824f3f8ec0
Cade Murphy <cademurphynz@gmail.com>
2016-06-30 10:39:46 +1200

    Purging temp user no longer gets rid of form templates

--- app/app/Paperwork/Baked/Authentication.php
+++ app/app/Paperwork/Baked/Authentication.php
@@ -154,9 +154,7 @@ class Authentication {
 			$app->sql->delete('client')->hard()->run();
 			$app->sql->delete('inventory')->hard()->run();
 			$app->sql->delete('job')->hard()->run();
-			$app->sql->delete('job_cache')->hard()->run();
 			$app->sql->delete('job_form')->hard()->run();
-			$app->sql->delete('job_form_template')->hard()->run();
 			$app->sql->delete('user_email_settings')->hard()->run();
 			
 			$app->event->log('Temporary user '.$app->user['username'].' data purged');
