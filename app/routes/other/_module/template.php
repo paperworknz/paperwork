e62024cd1bbaea956965bc358fecaf29fc8d5442
Cade Murphy <cademurphynz@gmail.com>
2016-07-22 14:31:18 +1200

    recursiveiterator not installed on server?

--- app/routes/other/_module/template.php
+++ app/routes/other/_module/template.php
@@ -5,17 +5,17 @@ $app->module->add('template', function($request) use ($app){
 	$path = '../public/css/media/icon/templates';
 	$themes = [];
 	
-	foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path)) as $filename => $file) {
-		$file = str_replace($path, '', $file);
-		$file = str_replace('\\', '/', $file);
-		$file = str_replace('/..', '/', $file);
-		$file = str_replace('/.', '/', $file);
-		if(substr($file, -1) != '/'){
-			$file = str_replace('.png', '', $file);
-			$file = str_replace('/', '', $file);
-			array_push($themes, $file);
-		}
-	}
+	// foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path)) as $filename => $file) {
+	// 	$file = str_replace($path, '', $file);
+	// 	$file = str_replace('\\', '/', $file);
+	// 	$file = str_replace('/..', '/', $file);
+	// 	$file = str_replace('/.', '/', $file);
+	// 	if(substr($file, -1) != '/'){
+	// 		$file = str_replace('.png', '', $file);
+	// 		$file = str_replace('/', '', $file);
+	// 		array_push($themes, $file);
+	// 	}
+	// }
 	
 	return [
 		'behaviors' => ['tab'],
