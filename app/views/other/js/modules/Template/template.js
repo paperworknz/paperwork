bab89cab6572b423dadaa6ceea7665694180d759
Cade Murphy <cademurphynz@gmail.com>
2016-08-28 14:10:39 +1200

    Added color palette to template properties

--- app/views/other/js/modules/Template/template.js
+++ app/views/other/js/modules/Template/template.js
@@ -20,9 +20,23 @@ Core.addModule('template', function(context){
 	context.use('tab');
 	context.use('document');
 	
+	var colors = ['#1abc9c', '#16a085', '#2ecc71', '#27ae60', '#3498db', '#2980b9', 
+	'#9b59b6', '#8e44ad', '#34495e', '#2c3e50', '#f1c40f', '#f39c12', '#e67e22',
+	'#d35400', '#e74c3c', '#c0392b', '#ecf0f1', '#bdc3c7', '#95a5a6', '#7f8c8d', 'white', 'black'];
+	
 	// Bind
+	construct();
 	bind();
 	getProperties();
+	
+	function construct(){
+		
+		for(let i in colors){
+			const value = colors[i];
+			
+			$body.find('.color-palette').append(`<div class="color" data-color="${value}" style="background-color: ${value}"></div>`);
+		}
+	}
 	
 	function bind(){
 		
@@ -107,6 +121,18 @@ Core.addModule('template', function(context){
 			
 			if(prop.indexOf('colour') != -1) return;
 			$body.find(`[data-property="${prop}"]`).css('background-color', '');
+		});
+		
+		// color palette click
+		$body.on('click', '.color-palette .color', function(){
+			var color = $(this).data('color'),
+				property = $(this).closest('[data-type="row"]').find('[data-type="key"]').text().trim();
+			
+			$(this).closest('[data-type="row"]').find('.prop').html(color);
+			
+			properties[property] = color;
+			saveProperties();
+			render();
 		});
 		
 		// Save properties
