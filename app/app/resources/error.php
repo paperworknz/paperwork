12629a0334f53ea626c72fd780b9df47b3451c91
Cade Murphy <cademurphynz@gmail.com>
2016-08-31 14:33:39 +1200

    Improved 404 page for SEO purposes

--- app/app/resources/error.php
+++ app/app/resources/error.php
@@ -8,3 +8,8 @@ $app->error(function($e) use ($app){
 	$app->flash('error', 'Something went wrong... Please try again. Otherwise, <b>if it\'s urgent please get in touch</b>. We are sorry for the inconvenience!');
 	$app->redirect($app->root.'/app');
 });
+
+$app->notFound(function() use ($app){
+	
+	$app->build->page('views/404.html');
+});
