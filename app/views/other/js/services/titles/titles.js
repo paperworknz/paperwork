cfba55dc570dda44b7f1ecc427ee0acf7b5813cc
Cade Murphy <cademurphynz@gmail.com>
2016-09-06 16:27:05 +1200

    Live Titles - New service "titles" - renders a useful title label for any element in the body with [data-title]

---
+++ app/views/other/js/services/titles/titles.js
@@ -0,0 +1,38 @@
+Core.addService('titles', function(){
+	
+	var $body = Paperwork.body;
+	
+	$body.on('mouseover', '[data-title]', function(){
+		render($(this));
+	});
+	
+	$body.on('mouseout', '[data-title]', remove);
+	
+	function render(request){
+		
+		var element,
+			title = request.data('title');
+		
+		if(!$body.find('#title').length) $body.append(`<div id="title"><div class="title_text"></div><div class="title_triangle"></div></div>`);
+		
+		element = $body.find('#title');
+		
+		// Render title
+		element.find('.title_text').html(title);
+		
+		// Position
+		element
+			.css({
+				top: (request.offset().top - element.outerHeight()),
+				left: request.offset().left + (request.outerWidth() / 2) - (element.outerWidth() / 3),
+			});
+		
+		// Display
+		element.show();
+	}
+	
+	function remove(){
+		
+		$body.find('#title').hide();
+	}
+});
