f3269585c8bb77817ba65e57a7c4d3ca9d1213bc
Cade Murphy <cademurphynz@gmail.com>
2016-09-08 09:38:46 +1200

    Improved hello module

--- app/views/other/js/modules/hello/hello.js
+++ app/views/other/js/modules/hello/hello.js
@@ -4,53 +4,45 @@ Core.addModule('hello', function(context){
 	
 	var i = 0;
 	var body_height = 0;
-	var messages = {
-		0: {
-			title: 'Welcome to Paperwork',
-			body:
-			``,
-			fn: function(){
-				
-			},
-		},
-	};
+	var request = {};
 	
-	render();
 	run();
 	
 	function run(){
 		$body.on('click', '[data-type="next-step"]', function(event){
+			
 			i++;
+			
 			$body.find('[data-type="parent"]').animate({
 				opacity: 0,
-			}, 100, function(){
-				render();
-				$body.find('[data-type="next-step"]').html('GOT IT');
-				
-				let new_height = $body.outerHeight();
-				
-				$body.css('height', body_height);
-				$body.animate({
-					height: new_height
-				}, 200, 'swing', function(){
-					$body.find('[data-type="parent"]').animate({
-						opacity: 1,
-					}, 100);
-				});
-			});
+			}, 100, render);
 		});
 	}
 	
 	function render(){
-		if(!messages[i]){
-			if(sessionStorage) sessionStorage.introduction = 'true';
-			return context.stop();
+		
+		let new_height;
+		
+		if(i > 1){
+			
+			return;
 		}
 		
 		$body.css('height', '');
 		body_height = $body.outerHeight();
 		
-		$body.find('[data-type="title"]').html(messages[i].title);
-		// $body.find('[data-type="view"]').html(messages[i].body);
+		$body.find('[data-type="one"]').hide();
+		$body.find('[data-type="two"]').show();
+		
+		new_height = $body.outerHeight();
+		$body.css('height', body_height);
+		
+		$body.animate({
+			height: new_height
+		}, 200, 'swing', function(){
+			$body.find('[data-type="parent"]').animate({
+				opacity: 1,
+			}, 100);
+		});
 	}
 });
