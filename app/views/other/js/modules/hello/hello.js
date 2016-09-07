653955906b264a5fb0992a359dd310d45e4ba391
Cade Murphy <cademurphynz@gmail.com>
2016-09-08 10:27:26 +1200

    Improved hello module - Temporary users now land on /hello

--- app/views/other/js/modules/hello/hello.js
+++ app/views/other/js/modules/hello/hello.js
@@ -1,48 +1,4 @@
 Core.addModule('hello', function(context){
 	
 	var $body = context.element;
-	
-	var i = 0;
-	var body_height = 0;
-	var request = {};
-	
-	run();
-	
-	function run(){
-		$body.on('click', '[data-type="next-step"]', function(event){
-			
-			i++;
-			
-			$body.find('[data-type="parent"]').animate({
-				opacity: 0,
-			}, 100, render);
-		});
-	}
-	
-	function render(){
-		
-		let new_height;
-		
-		if(i > 1){
-			
-			return;
-		}
-		
-		$body.css('height', '');
-		body_height = $body.outerHeight();
-		
-		$body.find('[data-type="one"]').hide();
-		$body.find('[data-type="two"]').show();
-		
-		new_height = $body.outerHeight();
-		$body.css('height', body_height);
-		
-		$body.animate({
-			height: new_height
-		}, 200, 'swing', function(){
-			$body.find('[data-type="parent"]').animate({
-				opacity: 1,
-			}, 100);
-		});
-	}
 });
--- public/js/modules/hello.js
+++ public/js/modules/hello.js
@@ -1,1 +1,1 @@
-"use strict";Core.addModule("hello",function(t){function e(){n.on("click",'[data-type="next-step"]',function(t){a++,n.find('[data-type="parent"]').animate({opacity:0},100,i)})}function i(){var t=void 0;a>1||(n.css("height",""),o=n.outerHeight(),n.find('[data-type="one"]').hide(),n.find('[data-type="two"]').show(),t=n.outerHeight(),n.css("height",o),n.animate({height:t},200,"swing",function(){n.find('[data-type="parent"]').animate({opacity:1},100)}))}var n=t.element,a=0,o=0;e()});
+"use strict";Core.addModule("hello",function(e){e.element});
