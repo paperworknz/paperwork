30733761dea879bb4db0582ab6572b82a8c32971
Cade Murphy <cademurphynz@gmail.com>
2016-09-15 17:20:00 +1200

    Improved new user flow

--- app/routes/other/put/settings.php
+++ app/routes/other/put/settings.php
@@ -72,7 +72,7 @@ $app->post('/put/settings', 'uac', function() use ($app){
 		case 'region': $app->redirect($app->root.'/onboard/user');
 		case 'user': $app->redirect($app->root.'/onboard/jobs');
 		case 'jobs': 
-			$app->flash('success', 'Settings updated. This <b>Templates</b> page is where you design your templates.');
-			$app->redirect($app->root.'/templates');
+			$app->flash('success', 'Welcome to Paperwork! Get started by making your first job!');
+			$app->redirect($app->root.'/jobs');
 	}
 });
